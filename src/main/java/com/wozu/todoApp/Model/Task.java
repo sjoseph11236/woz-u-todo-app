a77a6bc467604971ec88800d95a8d33285309b89
SayeedJ <sayeedjoseph@gmail.com>
2020-11-12 10:29:07 -0500

    add task model

--- src/main/java/com/wozu/todoApp/Model/Task.java
+++ src/main/java/com/wozu/todoApp/Model/Task.java
@@ -1,4 +1,62 @@
 package com.wozu.todoApp.Model;
 
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name="tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "todoList_id")
+    private TodoList todoList;
+    private String description;
+    private Boolean isComplete = false;
+
+    public Task() {}
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TodoList getTodoList() {
+        return todoList;
+    }
+
+    public void setTodoList(TodoList todoList) {
+        this.todoList = todoList;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Boolean getIsComplete() {
+        return isComplete;
+    }
+
+    public void setIsComplete(Boolean isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", todoList=" + todoList +
+                ", description='" + description + '\'' +
+                ", isComplete=" + isComplete +
+                '}';
+    }
 }
