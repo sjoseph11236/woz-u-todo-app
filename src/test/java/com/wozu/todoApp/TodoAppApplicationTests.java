469a919a23584732114cfbcd7be1950e6a0227ea
LionelBeato <lionel.beato@gmail.com>
2020-11-20 18:55:54 -0500

    yyup

--- src/test/java/com/wozu/todoApp/TodoAppApplicationTests.java
+++ src/test/java/com/wozu/todoApp/TodoAppApplicationTests.java
@@ -3,13 +3,13 @@ package com.wozu.todoApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-
-@TestPropertySource("/test.properties")
-@SpringBootTest
-class TodoAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//
+////@TestPropertySource("/test.properties")
+//@SpringBootTest
+//class TodoAppApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
