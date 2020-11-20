fb30aee6b5aa9d9452aec9f5cc674a1f6ff09c6d
LionelBeato <lionel.beato@gmail.com>
2020-11-20 18:10:59 -0500

    adds test.props

--- src/test/java/com/wozu/todoApp/TodoAppApplicationTests.java
+++ src/test/java/com/wozu/todoApp/TodoAppApplicationTests.java
@@ -2,7 +2,9 @@ package com.wozu.todoApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource("classpath:/test.properties")
 @SpringBootTest
 class TodoAppApplicationTests {
 
