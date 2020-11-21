5391bf8aa478ed6249b8e60ccbcc4de921837628
Lionel Beato <lionel.beato@gmail.com>
2020-11-20 21:22:56 -0500

    add secrets

--- src/test/java/com/wozu/todoApp/TodoAppApplicationTests.java
+++ src/test/java/com/wozu/todoApp/TodoAppApplicationTests.java
@@ -2,14 +2,15 @@ package com.wozu.todoApp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.test.context.TestPropertySource;
-//
-////@TestPropertySource("/test.properties")
-//@SpringBootTest
-//class TodoAppApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+
+@TestPropertySource("classpath:/test.properties")
+@SpringBootTest
+class TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
